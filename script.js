fcd5f76c125cb33bb29769f6ba897b5f29a5171e
PizzaMentor <pizzzamentor@gmail.com>
2023-08-03 16:40:15 +0100

    added arrays

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function generateRandomNumber(num) {
     return Math.floor(Math.random() * num)
   }
+
+  const options = {
+    starters: ['Jiaozi (Gyoza)', 'Caprese Salad', 'Grilled Lobster Tails', 'Somosa', 'Tteokbooki', 'Salt & Pepper Squid', 'Prawn Pil Pil', 'Thai Fishcake', 'Falafel', 'Manti'],
+    mains: ['Chili Con Carne', 'Mac & Cheese', 'Beef Stew', 'Margherita Pizza', 'Steak', 'Tandoori Chicken', 'Pad Thai', 'Spinach Calzone', 'Poke Bowl', 'Fajitas'],
+    desserts: ['Shrikand', 'Baklava', 'Lime Cheesecake', 'Raspberry Brûlée', 'Bungeoppang', 'Churros', 'Sticky Toffee Pudding', 'Ice Cream Sunday', 'Eclairs', 'Beignet']
+  }
+  
